6e0825ae4319e548716e86d59b311bba5c9e59f3
Eduardo Castillo <lincidro@gmail.com>
2020-11-24 14:13:21 -0500

    feat: se separa la lista de clientes a un json.ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,20 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'Juan', apellido: 'Arias', email: 'correo1@test.com', createAt: '2017-11-18' },
-    { id: 2, nombre: 'Maria', apellido: 'Perez', email: 'correo2@test.com', createAt: '2017-11-15' },
-    { id: 3, nombre: 'Mateo', apellido: 'Contreras', email: 'correo3@test.com', createAt: '2017-11-18' },
-    { id: 4, nombre: 'Lucia', apellido: 'Lujan', email: 'correo4@test.com', createAt: '2017-11-13' },
-    { id: 5, nombre: 'Sheyla', apellido: 'Correa', email: 'correo5@test.com', createAt: '2017-11-18' },
-    { id: 6, nombre: 'Tecia', apellido: 'Jara', email: 'correo6@test.com', createAt: '2017-11-13' },
-    { id: 7, nombre: 'Gerardo', apellido: 'Torres', email: 'correo7@test.com', createAt: '2017-11-13' },
-    { id: 8, nombre: 'Felix', apellido: 'Lopez', email: 'correo8@test.com', createAt: '2017-11-15' },
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Juan', apellido: 'Arias', email: 'correo1@test.com', createAt: '2017-11-18' },
+  { id: 2, nombre: 'Maria', apellido: 'Perez', email: 'correo2@test.com', createAt: '2017-11-15' },
+  { id: 3, nombre: 'Mateo', apellido: 'Contreras', email: 'correo3@test.com', createAt: '2017-11-18' },
+  { id: 4, nombre: 'Lucia', apellido: 'Lujan', email: 'correo4@test.com', createAt: '2017-11-13' },
+  { id: 5, nombre: 'Sheyla', apellido: 'Correa', email: 'correo5@test.com', createAt: '2017-11-18' },
+  { id: 6, nombre: 'Tecia', apellido: 'Jara', email: 'correo6@test.com', createAt: '2017-11-13' },
+  { id: 7, nombre: 'Gerardo', apellido: 'Torres', email: 'correo7@test.com', createAt: '2017-11-13' },
+  { id: 8, nombre: 'Felix', apellido: 'Lopez', email: 'correo8@test.com', createAt: '2017-11-15' },
+];
